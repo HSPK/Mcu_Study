141ea72f936237a171776b70691006ad04b29b85
root <root@localhost>
2017-04-16 22:52:15 +0800

    完善

--- main.c
+++ main.c
@@ -3,21 +3,53 @@ typedef unsigned char uint8;
 typedef unsigned int  uint16;
 typedef char int8;
 typedef int  int16;
-int8 code musicArray[]={};
-int16 code soundFrequ[]={262,};
-int16 code tmr0reload[]={
-		65536 -(12000000\12)\(262*2),
-		65536 -(12000000\12)\(262*2),
-		65536 -(12000000\12)\(262*2),
-		65536 -(12000000\12)\(262*2),
-		65536 -(12000000\12)\(262*2),
-		65536 -(12000000\12)\(262*2),
-		65536 -(12000000\12)\(262*2)
+/*uint8 code musicArray[]={0,1,0,1,5,1,	1,3,	1,2,2,1,3,1,	5,1,3,1,3,1,	3,2,5,1,	1,3,	2,1,1,1,6,1,
+						 5,3,	5,2,5,1,	1,3,	1,2,2,1,3,1,	6,1,3,1,5,1,	5,1,3,1,2,1,	1,3,	2,2,5,1,
+						 3,1,2,2,	2,2,3,1,	5,3,	5,1,3,1,5,1,	6,1,1,1,6,1,	6,2,3,1,	2,2,1,1,	2,2,3,1,
+						 3,3,	3,2,5,1,	5,3,	0,1,3,1,3,1,	2,1,1,1,1,1,	1,2,5,1,	2,2,1,1,	3,1,2,1,1,3,0,1}; 	*/					  //δ����
+/*uint8 code musicArray[]={1,2,2,2,	3,2,1,2,	1,2,2,2,	3,2,1,2,	3,2,4,2,5,2,0,2,	3,2,4,2,5,2,0,2,
+						 5,1,6,1,5,1,4,1,3,2,1,2,	5,1,6,1,5,1,4,1,3,2,1,2,	2,2,8,2,1,2,0,2,	2,2,8,2,1,2,0,2};    //�ֻ�ϻ�			 */
+
+uint8 code musicArray[]={
+						0,1,0,1,5,1,	1,3,	1,2,2,1,3,1,	5,1,3,1,3,1,	3,2,5,1,	1,3,	2,1,1,1,6,1,
+						 5,3,	5,2,5,1,	1,3,	1,2,2,1,3,1,	6,1,3,1,5,1,	5,1,3,1,2,1,	1,3,	2,2,5,1,
+						 3,1,2,2,	2,2,3,1,	5,3,	5,1,3,1,5,1,	6,1,1,1,6,1,	6,2,3,1,	2,2,1,1,	2,2,3,1,
+						 3,3,	3,2,5,1,	5,3,	0,1,3,1,3,1,	2,1,1,1,1,1,	1,2,5,1,	2,2,1,1,	3,1,2,1,1,3,0,9,
+						 
+						 
+						 1,2,2,2,	3,2,1,2,	1,2,2,2,	3,2,1,2,	3,2,4,2,5,2,0,2,	3,2,4,2,5,2,0,2,
+						 5,1,6,1,5,1,4,1,3,2,1,2,	5,1,6,1,5,1,4,1,3,2,1,2,	2,2,8,2,1,2,0,2,	2,2,8,2,1,2,0,9,
+						 
+						  
+
+
+
+
+
+
+
+
+
+
+
+1,1,1,1,5,1,5,1,	6,1,6,1,5,2,	4,1,4,1,3,1,3,1,	2,1,2,1,1,2,	5,1,5,1,4,1,4,1,	3,1,3,1,2,2,5,1,5,1,4,1,4,1,	3,1,3,1,2,2,	1,1,1,1,5,1,5,1,	6,1,6,1,5,2,	4,1,4,1,3,1,3,1,	2,1,2,1,1,2	};
+//uint16 code soundFrequ[]={000,262,294,330,350,392,440,494};
+uint16 code soundFrequ[]={000,1047,1175,1319,1397,1568,1760,1976,784};
+uint16 code tmr0reload[]={
+      	65535,
+		65536 -(12000000/12)/(1047*2),
+		65536 -(12000000/12)/(1175*2),
+		65536 -(12000000/12)/(1319*2),
+		65536 -(12000000/12)/(1397*2),
+		65536 -(12000000/12)/(1568*2),
+		65536 -(12000000/12)/(1760*2),
+		65536 -(12000000/12)/(1976*2),
+		65536 -(12000000/12)/(784*2)
 };
 bit tmrFlag = 0;
 bit enable  = 0;
-uint8 T0RH,T0RL;
-sbit beep P2^6;          // define fengmingqi io
+uint8 T0RH=0xff,T0RL=0xfe;
+sbit beep=P1^5;          // define fengmingqi io
 void delayms(uint16 ms);
 void playMusic();
 void Init();
@@ -37,6 +69,7 @@ void Init()
 	TMOD = 0X01;      //tmr0 init 
 	TH0 = T0RH;
 	TL0 = T0RL;
+	TR0=1;
 	EA = 1;
 	ET0 = 1;
 }
@@ -44,36 +77,53 @@ void Init()
 
 void playMusic()
 {
-	uint8 musicCon,j;
-	uint16 time,soundTime,beatTime;
-for(musicCon=0;musicCon<(sizeof(musicArray[])>>1);)
+	int musicCon=-1;
+	uint16 time=0,soundTime=0,beatTime=0;
+	for(musicCon=-1;musicCon<(sizeof(musicArray)+1);)
   {
 	while(!tmrFlag);
 	tmrFlag = 0;
 	if(time==beatTime)
 	{
-		musicCon++;
+		musicCon+=2;
+		time = 0;
+		if (!musicArray[(musicCon-1)])
+		{
+			enable = 0;
+			T0RH = 0XD8;
+			T0RL = 0XF0;
+			beatTime = 30*musicArray[musicCon];
+			enable = 0;
+		}
+		else
+		{
 		T0RH =tmr0reload[musicArray[(musicCon-1)]]>>8;
 		T0RL =tmr0reload[musicArray[(musicCon-1)]];
 		beatTime  = soundFrequ[musicArray[(musicCon-1)]]*2*musicArray[musicCon];
-		soundTime = (beatTime>>3)*3;
+		beatTime = beatTime>>1;
+		soundTime = ((beatTime*3)>>2);
 		enable = 1;
+		}
 	}
 	else if(time==soundTime)
 	{
-			enable=0;
+		enable=0;
+	}
+	time++;
   }
+  enable=0;
 }
 
 
-void delayms(int16 ms)
+void delayms(uint16 ms)
 {
-	uint8 i,j;
+	uint16 i;
+	uint8 j;
 	for(i=0;i<ms;i++)
 		for(j=0;j<128;j++);
 }
 
-void tmr0interrupt() interrupt 0
+void tmr0interrupt() interrupt 1
 {
 	tmrFlag = 1;
 	TH0=T0RH;
