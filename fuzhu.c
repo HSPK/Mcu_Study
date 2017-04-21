1c1b1d6fbf41916af96f98fc0c750cc1e66b1b6e
root <root@localhost>
2017-04-21 12:49:53 +0800

    haha

---
+++ fuzhu.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+	unsigned int musicArray[]={150,151,152,153,151,	151,152,153,151,	153,154,255,	153,154,255,	155,156,155,154,
+						  253,251,		155,156,155,154,	253,251,	252,245,251,250,	252,245,251,250,150
+}; 
+	unsigned int sum;
+    sum=sizeof(sum);
+  //  sum=sizeof("a");
+	printf("%d",sum);
+}
